040d3d4318a38e0148cd797cf71f20f2d5e4ef8b
Alan Christian <alanchristiancreator@hotmail.com>
2021-03-02 20:40:32 -0300

    Hooks Authenticat

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Container, Logo, Form, FormTitle } from './styles';
 
 const Signin: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const { signIn } = useAuth();
+  
     return (
       <Container>
         <Logo>
@@ -15,18 +22,20 @@ const Signin: React.FC = () => {
           <h2>Minha Carteira</h2>
         </Logo>
 
-        <Form onSubmit={() => {}}> 
+        <Form onSubmit={() => signIn(email, password)}> 
           <FormTitle>Entrar</FormTitle>
 
           <Input 
             type="email"
             placeholder="Seu e-mail"
             required
-          />
+            onChange={(e) => setEmail(e.target.value)}
+            />
           <Input
             type="password" 
             placeholder="Sua senha"
             required
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <Button type="submit">Acessar conta</Button>
