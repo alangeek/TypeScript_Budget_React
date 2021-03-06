f8c8bf87d69a3be600d2aaf82a72f0db4e87864c
Alan Christian <alanchristiancreator@hotmail.com>
2021-03-05 21:35:06 -0300

    media queries

--- src/components/PieChartBox/styles.ts
+++ src/components/PieChartBox/styles.ts
@@ -16,6 +16,11 @@ export const Container = styled.div`
   border-radius: 7px;
 
   display: flex;
+
+  @media(max-width: 770px) {
+    display: flex;
+    width: 100%;
+  }
 `;
 
 export const SideLeft = styled.aside`
@@ -23,6 +28,21 @@ export const SideLeft = styled.aside`
 
   > h2 {
     margin-bottom: 20px;
+  }
+
+  @media(max-width: 1345px) {
+    padding: 0 15px 5px;
+    margin-bottom: 7px;
+
+    > h2 {
+      margin-top: 15px;
+      margin-bottom: 7px;
+    }
+  }
+
+  @media(max-width: 420px) {
+    padding: 15px;
+    margin-bottom: 7px;
   }
 `;
 
@@ -45,6 +65,11 @@ export const LegendContainer  = styled.ul`
   ::-webkit-scrollbar-track {
     background-color: ${props => props.theme.colors.black};
     border-radius: 10px;
+  }
+
+  @media(max-width: 1345px) {
+    display: flex;
+    flex-direction: column;
   }
 `;
 
