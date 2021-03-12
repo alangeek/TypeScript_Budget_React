4cb5d758d5a4827172388a2836bc76cdefca1c17
Alan Christian <alanchristiancreator@hotmail.com>
2021-03-12 19:52:09 -0300

    effect of entry in the balance history

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,9 +1,23 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 
 interface ILegendProps {
   color: string;
 } 
+
+const animate = keyframes`
+  0% {
+    transform: translateX(-100px);
+    opacity: 0;
+  }
+  50% {
+    opacity: .3;
+  }
+  100% {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+`;
 
 export const Container = styled.div`
   width: 100%;
@@ -19,7 +33,7 @@ export const Container = styled.div`
 
   border-radius: 7px;
 
-  
+  animation: ${animate} .9s;
 `;
 
 export const ChartContainer = styled.div`
